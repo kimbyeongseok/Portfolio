cdb99e1c3e16c91732db582eec104ca4134b1c13
kimbyeongseok <69345685+kimbyeongseok@users.noreply.github.com>
2020-09-29 10:01:22 +0900

    header layout design

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  $(".side_nav_bars").click(function () {
+    $(this).toggleClass("open");
+    $(".sub_header").toggleClass("active");
+    $(".main_header").toggleClass("active");
+  });
+});
+// text animation function
+document.getElementById("home").mouseover = AnimatedText();
+
+function AnimatedText(
+  target,
+  texts,
+  changeInterval,
+  updateInterval,
+  onTextChanged
+) {
+  var currentText = parseInt(Math.random() * texts.length);
+  var areaText = texts[0];
+  this.t1 = setInterval(
+    function () {
+      var c = parseInt(
+        Math.random() * Math.max(texts[currentText].length, areaText.length)
+      );
+      var s = texts[currentText][c];
+      if (typeof s == "undefined") s = " ";
+      while (areaText.length < c) areaText += " ";
+      var newText = (areaText.slice(0, c) + s + areaText.slice(c + 1)).trim();
+      var diff = !(newText == areaText);
+      areaText = newText;
+      if (onTextChanged && diff) onTextChanged();
+      target.innerHTML = areaText.length == 0 ? "&nbsp;" : areaText;
+    }.bind(this),
+    updateInterval ? updateInterval : 50
+  );
+  this.t2 = setInterval(
+    function () {
+      currentText = parseInt(Math.random() * texts.length);
+    }.bind(this),
+    changeInterval ? changeInterval : 400
+  );
+}
+AnimatedText.prototype = {
+  constructor: AnimatedText,
+  stop: function () {
+    clearInterval(this.t1);
+    clearInterval(this.t2);
+  },
+};
+new AnimatedText(document.getElementById("home"), [
+  "KIM BYEONG SEOK",
+  "kIm ByeOnG sEOk",
+  "KiM byEONg SeOK",
+  "KIM bYEong seok",
+  "KIM BYEONG SEOK",
+]);
